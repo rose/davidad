a6a1927ff70642342f3072875d89068eb887d662
Rose Ames <rose@happyspork.com>
2014-06-09 06:34:57 -0400

    replace single instances of Chuck & Norris

--- src.js
+++ src.js
@@ -5,13 +5,30 @@ var subs = [
   [/\bChuck Norris\b/gi, "Davidad"]
 ];
 
+var allSubs = {
+  "chuck" : "David",
+  "norris" : "Dalrymple"
+};
+
+var davidadPage = false;
+
 var process_all = function(node) {
   var child, next;
   var type = node.nodeType;
   if (type == 3) {
     var txt = node.nodeValue;
-    for (var i = 0; i < subs.length; i++) {
-      txt = txt.replace(subs[i][0], subs[i][1]);
+    txt = txt.replace(/\bChuck Norris\b/gi, function(match) {
+      davidadPage = true;
+      return "Davidad";
+    });
+    if (davidadPage) {
+      txt = txt.replace(/\b(Chuck|Norris)\b/gi, function(match) {
+        var lowMatch = match.toLowerCase()
+        if (allSubs[lowMatch]) {
+          return allSubs[lowMatch];
+        }
+        return match;
+      });
     }
     node.nodeValue = txt;
   } else if (type == 1 || type == 9 || type == 11) {
